3d9998881b2844a0a799888318668b6f8bdc979d
evasimpetru28 <eva.simpetru@gmail.com>
2019-05-25 11:31:07 +0300

    Lista de biserici

--- src/main/java/ro/esolutions/PopeIsComing/ConectorExtern.java
+++ src/main/java/ro/esolutions/PopeIsComing/ConectorExtern.java
@@ -1,25 +1,65 @@
 package ro.esolutions.PopeIsComing;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ConectorExtern {
 
-    @GetMapping("/churches")
-    public String getAllChurches(){
+    @GetMapping("/churches/{month}")
+    public String getAllChurches(@PathVariable String month){
+
+        String raspuns = "Bisericile mele sunt: ";
+
+        List<Biserica> biserici = new ArrayList<>(); // <Biserica> este clasa obiectelor din lista, biserici este numele listei
 
 
-        //de facut o biserica noua
-
-        //in loc de return un text chior, return "... sunt" + numele bisericii;
         Biserica SfMaria = new Biserica();
         SfMaria.nume = "Biserica Sf Maria";
         SfMaria.lunaDeVizita = "iulie";
         SfMaria.tipReligie = "ortodoxa";
 
-        return "Biserica mea este: " + SfMaria.nume;
+        biserici.add(SfMaria);
+
+        Biserica treiSfinti = new Biserica();
+        treiSfinti.nume = "Biserica Trei Sfinti";
+        treiSfinti.lunaDeVizita = "octombrie";
+        treiSfinti.tipReligie = "catolica";
+
+        biserici.add(treiSfinti);
+
+        Biserica sfintiiApostoli = new Biserica();
+        sfintiiApostoli.nume = "Catedrala Sfintii Aposotli";
+        sfintiiApostoli.lunaDeVizita = "aprilie";
+        sfintiiApostoli.tipReligie = "ortodoxa";
+
+        biserici.add(sfintiiApostoli);
+
+        Biserica constantinSiElena = new Biserica();
+        constantinSiElena.nume = "Biserica Sfintii Constantin si Elena";
+        constantinSiElena.tipReligie = "ortodoxa";
+        constantinSiElena.lunaDeVizita = "martie";
+
+        biserici.add(constantinSiElena);
+
+        Biserica SfAndrei = new Biserica();
+        SfAndrei.nume = "Biserica Sfantul Andrei";
+        SfAndrei.tipReligie = "catolica";
+        SfAndrei.lunaDeVizita = "aprilie";
+
+        biserici.add(SfAndrei);
+
+        for (int i = 0; i < biserici.size(); i ++) {
+            if (biserici.get(i).lunaDeVizita == month)
+                raspuns += biserici.get(i).nume + ", ";
+        }
+
+
+        return raspuns;
     }
 
 }
-
